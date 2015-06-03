a75f575efddb5b81f1216519e53a3c00279a5019
CONGIO-HOLLARD Jorane <congiohj@hostname.iut2.upmf-grenoble.fr>
2015-06-03 13:15:05 +0200

    e

--- m2105_ihm_tp5/src/m2105_ihm/ui/BoiteDialogUI.java
+++ m2105_ihm_tp5/src/m2105_ihm/ui/BoiteDialogUI.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package m2105_ihm.ui;
 
 import javax.swing.JComboBox;
@@ -19,112 +18,111 @@ import m2105_ihm.nf.NoyauFonctionnel;
  * @author laurillau
  */
 public class BoiteDialogUI {
+
     /**
      * Boîte de dialogue pour confirmer la suppression d'un contact
+     *
      * @param c un contact
      * @return vrai si confirmé
      */
-	NoyauFonctionnel nf = ;
-	
-	
-    public static boolean afficherConfirmation(JFrame fenetre, Contact c) {
-        boolean res = false;
 
-        if (c != null) {
-            String [] choix = new String[] { "Supprimer", "Annuler" }; 
-            
-            Object selectedValue = JOptionPane.showOptionDialog(fenetre,
-                  "Voulez-vous vraiment supprimer le contact : " 
-                  + c.getPrenom() + " " + c.getNom() + " ?", 
-                  "Suppression d'un contact",
-                  JOptionPane.DEFAULT_OPTION,
-                  JOptionPane.QUESTION_MESSAGE, 
-                  null,
-                  choix,
-                  choix[1]);
-            res = (((Integer) selectedValue) == 0);
-        }
-        
-        return res;
+    public static boolean afficherConfirmation(JFrame fenetre, Contact c) {
+	boolean res = false;
+
+	if (c != null) {
+	    String[] choix = new String[]{"Supprimer", "Annuler"};
+
+	    Object selectedValue = JOptionPane.showOptionDialog(fenetre,
+		    "Voulez-vous vraiment supprimer le contact : "
+		    + c.getPrenom() + " " + c.getNom() + " ?",
+		    "Suppression d'un contact",
+		    JOptionPane.DEFAULT_OPTION,
+		    JOptionPane.QUESTION_MESSAGE,
+		    null,
+		    choix,
+		    choix[1]);
+	    res = (((Integer) selectedValue) == 0);
+	}
+
+	return res;
     }
 
     /**
      * Boîte de dialogue pour confirmer la suppression d'un groupe de contacts
+     *
      * @param g un groupe de contacts
      * @return vrai si confirmé
-     */    
+     */
     public static boolean afficherConfirmation(JFrame fenetre, GroupeContacts g) {
-        boolean res = false;
+	boolean res = false;
 
-                if (g != null) {
-            String [] choix = new String[] { "Supprimer", "Annuler" }; 
-            
-            Object selectedValue = JOptionPane.showOptionDialog(fenetre,
-                  "Voulez-vous vraiment supprimer le groupe : " 
-                  + g.getNom() +" ?", 
-                  "Suppression d'un contact",
-                  JOptionPane.DEFAULT_OPTION,
-                  JOptionPane.QUESTION_MESSAGE, 
-                  null,
-                  choix,
-                  choix[1]);
-            
-            res = (((Integer) selectedValue) == 0);
-        }
-        
-       
-        return res;
+	if (g != null) {
+	    String[] choix = new String[]{"Supprimer", "Annuler"};
+
+	    Object selectedValue = JOptionPane.showOptionDialog(fenetre,
+		    "Voulez-vous vraiment supprimer le groupe : "
+		    + g.getNom() + " ?",
+		    "Suppression d'un contact",
+		    JOptionPane.DEFAULT_OPTION,
+		    JOptionPane.QUESTION_MESSAGE,
+		    null,
+		    choix,
+		    choix[1]);
+
+	    res = (((Integer) selectedValue) == 0);
+	}
+
+	return res;
     }
 
     /**
      * Boîte de dialogue choisir un groupe auquel ajouter un contact
+     *
      * @param titre titre de la fenêtre
      * @param groupes liste des groupes existants
      * @return groupe choisi sinon valeur null
-     */    
-    public static GroupeContacts afficherChoixMembreContact(JFrame fenetre, String titre, GroupeContacts [] groupes) {
-        String [] test = new String [groupes.length];
-    	
-    	GroupeContacts res = null;
-    	for ( int i=0; i<groupes.length;i++)
-    	{
-    		
-    		test[i] = groupes[i].getNom();
+     */
+    public static GroupeContacts afficherChoixMembreContact(JFrame fenetre, String titre, GroupeContacts[] groupes) {
+	String[] nomGroupes = new String[groupes.length];
 
-    	}
-    	
-        if (titre == null) { titre = ""; }
-                        if (groupes != null) {
-           
-            
-            
-             Object selectedValue =  JOptionPane.showInputDialog(fenetre, "Dans quel groupe voulez vous ajoutez le contact : ", " ", JOptionPane.QUESTION_MESSAGE, null  ,test, 1);
-      
-            res = ;
-           
-                      
-        }
-        
-        return res;
+	GroupeContacts res = null;
+	for (int i = 0; i < groupes.length; i++)
+	{
+	    nomGroupes[i] = groupes[i].getNom();
+	}
+
+	if (titre == null) {
+	    titre = "";
+	}
+	if (groupes != null) {
+
+	    Object selectedValue = JOptionPane.showInputDialog(fenetre, "Dans quel groupe voulez vous ajoutez le contact : ", " ", JOptionPane.QUESTION_MESSAGE, null, nomGroupes, 1);
+
+	    for (int i = 0; i < groupes.length; i++) {
+		if (nomGroupes[i] == selectedValue) {
+		    res = groupes[i];
+		}
+	    }
+	}
+
+	return res;
     }
-    
-    public static Evenement afficherChoixEvenement(JFrame fenetre, String titre, Evenement [] e) {
-        Evenement res = null;
-        if (titre == null) { titre = ""; }
-        JComboBox combo = new JComboBox(e);
-                        if (e != null) {
-           
-            
-            
-             Object selectedValue =  JOptionPane.showInputDialog(fenetre, "Quel evenement voulez vous supprimer : ", " ", JOptionPane.QUESTION_MESSAGE, null , e, 1);
-      
-            
-            res = ((Evenement) selectedValue);
-           
-                      
-        }
-        
-        return res;
+
+    public static Evenement afficherChoixEvenement(JFrame fenetre, String titre, Evenement[] e) {
+	Evenement res = null;
+	if (titre == null) {
+	    titre = "";
+	}
+	JComboBox combo = new JComboBox(e);
+	if (e != null) {
+
+	    Object selectedValue = JOptionPane.showInputDialog(fenetre, "Quel evenement voulez vous supprimer : ", " ", JOptionPane.QUESTION_MESSAGE, null, e, 1);
+
+	    res = ((Evenement) selectedValue);
+
+	}
+
+	return res;
     }
-    
+
 }
